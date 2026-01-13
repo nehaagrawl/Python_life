395e433f5996395dd451d04246a4b305bd8afc60
neha agrawal <nehaagrawal1305@gmail.com>
2026-01-13 21:39:29 +0530

    datatypes

---
+++ basic/datatypes.py
@@ -0,0 +1,33 @@
+# primitive Data Types: # Integers # Floats # Booleans, and # Strings/
+# Non pre,imitive Data Types: # Lists # Tuples # Dictionaries, and # Sets
+
+a=10               # Integer
+b=20.5             # Float  
+c=True             # Boolean
+d="Hello, World!"  # String
+e=[1, 2, 3, 4, 5]  # List
+f=(10, 20, 30)     # Tuple
+g={"name": "Alice", "age": 25}  # Dictionary
+h={1, 2, 3, 4, 5}  # Set
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'float'>
+print(type(c))  # <class 'bool'>
+
+
+print(type(d))  # <class 'str'>
+print(type(e))  # <class 'list'>
+print(type(f))  # <class 'tuple'>
+print(type(g))  # <class 'dict'>
+print(type(h))  # <class 'set'>
+print(a, b, c, d, e, f, g, h)
+# Output:
+# <class 'int'>
+# <class 'float'>
+# <class 'bool'>
+# <class 'str'>
+# <class 'list'>
+# <class 'tuple'>
+# <class 'dict'>
+# <class 'set'>
+print(a, b, c, d, e, f, g, h)
+# 10 20.5 True Hello, World! [1, 2, 3, 4, 5] (10, 20, 30) {'name': 'Alice', 'age': 25} {1, 2, 3, 4, 5}
