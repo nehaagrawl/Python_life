d69b324018318b7d0ad4efbf0829e7de289e25e8
neha agrawal <nehaagrawal1305@gmail.com>
2026-01-14 22:08:16 +0530

    reverse_string

---
+++ basic/reverse_string.py
@@ -0,0 +1,12 @@
+def reverse_string(s):
+    reversed_string=""
+    for char in s:
+        reversed_string=char+reversed_string
+    return reversed_string
+
+print(reverse_string("lawarga ahen"))
+
+    
+
+
+
