e0b1ee25b645fbe2ac1411acea16e1a25658ee91
neha agrawal <nehaagrawal1305@gmail.com>
2026-01-27 20:26:33 +0530

    largest element in array

---
+++ basic/largest_element.py
@@ -0,0 +1,12 @@
+class LargestElement:
+
+    def arrays(__init__,arr):
+        max=arr[0]
+        for i in arr[1:]:
+            if i>max:
+                max=i
+        return max
+    
+obj=LargestElement()
+arr=[12,3,45,53335,234,2,1,9]
+print("largest element of array: ",obj.arrays(arr))
